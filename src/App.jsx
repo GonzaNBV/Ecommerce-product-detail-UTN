284bd9b4f09de9df693010dc8d41c513fe626c3a
Gonzalo Nicolas Vidal <gonzalonvidal64@gmail.com>
2025-04-04 12:31:48 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Components/styles/global.css";
+import Productos from "./Components/Productos/Productos";
+
+function App() {
+  return (
+    <div className="App">
+      <Productos />
+    </div>
+  );
+}
+
+export default App;
